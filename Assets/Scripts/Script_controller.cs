dacad6ff48687e71d25c72a94accae8cf8ff8e4d
Jani Haiko <ojaha065@edu.xamk.fi>
2019-01-01 15:28:51 +0200

    Initial Upload

---
+++ Assets/Scripts/Script_controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_controller : MonoBehaviour
+{
+    private GameObject FPS_laskuri;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPS_laskuri = GameObject.Find("FPS-laskuri");
+        FPS_laskuri.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            FPS_laskuri.SetActive(!FPS_laskuri.activeSelf);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+#if (UNITY_EDITOR)
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif (UNITY_STANDALONE)
+    Application.Quit();
+#elif (UNITY_WEBGL)
+        Application.OpenURL("about:blank");
+#endif
+        }
+#if (UNITY_EDITOR)
+        else if (Input.GetKey(KeyCode.P))
+        {
+            UnityEditor.EditorApplication.isPaused = true;
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Script_triggerKohde.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_triggerKohde : MonoBehaviour
+{
+    public int kohdeID = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            Debug.Log("Pelaaja törmäsi triggeriin");
+
+            switch (kohdeID)
+            {
+                case 0:
+                    Debug.Log("Tämä on kohde 0");
+                    break;
+                default:
+                    Debug.LogWarning("Pelaaja törmäsi kohteeseen jolla ei ole käsittelijää");
+                    break;
+            }
+        }
+    }
+}
